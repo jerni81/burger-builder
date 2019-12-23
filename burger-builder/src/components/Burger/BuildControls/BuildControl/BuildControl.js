00f4fd16a61fc71c898ff2c4c456508acfcc5e76
jerni81 <jerni81@yahoo.com>
2019-12-23 13:49:15 -0500

    add buttons working

--- burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,7 +4,7 @@ import classes from './BuildControl.css'
 const buildControl = (props) => (
   <div className="BuildControl">
     <div className="Label">{props.label}</div>
-    <button className="More">ADD</button>
+    <button onClick={props.added} className="More">ADD</button>
     <button className="Less">REMOVE</button>
   </div>
 )
